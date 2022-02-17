60ee27f774b286c05492a9fbe77c173d1dfbb2e3
Declan McLaughlin <82425277+DeclanJM@users.noreply.github.com>
2022-02-17 12:50:46 -0700

    First edits at the end of class.

--- src/StonkMarket.java
+++ src/StonkMarket.java
@@ -5,6 +5,10 @@ public class StonkMarket {
     private double exchangeRate = 1;
     private static final Random RANDY = new Random();
 
+public double getExchangeRate(){
+    return exchangeRate;
+}
+
     public double fazCoinToUSD(int fazCoin){
         return (fazCoin * getExchangeRate()) / 100.0;
     }
